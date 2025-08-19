cacc8542dad102d0265816677c9ef0552e982db6
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-19 19:10:30 +0300

    update entities

--- src/main/java/com/mini_banking/server/entity/Account.java
+++ src/main/java/com/mini_banking/server/entity/Account.java
@@ -15,14 +15,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "account")
 public class Account {
   @Id
--- src/main/java/com/mini_banking/server/entity/User.java
+++ src/main/java/com/mini_banking/server/entity/User.java
@@ -14,14 +14,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user")
 public class User {
   @Id
