8cdd5ce37186394ccf5be913f86763ba54a505ed
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-23 11:03:59 +0300

    update swagger config

--- src/main/java/com/mini_banking/server/config/SwaggerConfig.java
+++ src/main/java/com/mini_banking/server/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.mini_banking.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,7 +16,16 @@ public class SwaggerConfig {
   public OpenAPI openAPI() {
     return new OpenAPI()
         .info(new Info().title("Mini Banking API")
-            .description("Mini Banking Api's")
-            .version("1.0"));
+            .description("Mini Banking Api")
+            .version("1.0"))
+        .addSecurityItem(new SecurityRequirement()
+            .addList("Bearer Auth"))
+        .components(new Components()
+            .addSecuritySchemes("Bearer Auth",
+                new SecurityScheme()
+                    .name("Bearer Auth")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
   }
 }
